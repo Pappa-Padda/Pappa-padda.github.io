a188df91699bc9f7ccf2e98e1f5c011c72b3a2ee
Jean-Pierre Retief <jp.retief@potch.matogen.com>
2025-07-25 00:49:37 +0200

    Added flesh to the skeleton
    Almost done, just projects left

---
+++ assets/js/links.js
@@ -0,0 +1,9 @@
+window.siteLinks = {
+  profilePic: "https://raw.githubusercontent.com/Pappa-Padda/Pappa-padda.github.io/main/assets/ProfilePic.png",
+  github: "https://github.com/Pappa-Padda",
+  linkedin: "https://www.linkedin.com/in/jean-pierre-retief-a4a1a81a0/",
+  instagram: "https://www.instagram.com/jp.retief.90/",
+  youtube: "https://youtube.com/@jp-retief?si=1-TgVqGBMM8x60Qf",
+  facebook: "https://www.facebook.com/jp.retief.90/",
+  email: "mailto:jpretief84@gmail.com"
+};
